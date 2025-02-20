73b4ca13310ff6716f6f43332b69d04fde94e515
techatlasdev <srgoqui@gmail.com>
2025-02-19 21:36:29 -0500

    Ruta principal terminada!

--- backend/backend/aplicacion.py
+++ backend/backend/aplicacion.py
@@ -11,7 +11,15 @@ def prueba():
     return {"mensaje": "Hola mundo"} # El sistema nos dará el diccionario "mensaje": "Hola mundo"
 
 # Crear ruta de busqueda
-@backend.get("/buscar/") # http://backend/buscar
-def buscar(consulta):
+@backend.get("/buscar") # http://backend/buscar
+def buscar(consulta, resultados, cantidad):
+    URL_INTERMEDIARIO = "https://prl412-web-backend.vercel.app/api/search"
+    DATOS = {
+        "query": consulta,
+        "facets": resultados,
+        "limit": cantidad
+    }
+    respuesta = requests.post(url=URL_INTERMEDIARIO, data=DATOS)
+
     # Crear una variable que contenga la URL
-    return {"mensaje": f"Buscando {consulta}"} # El sistema nos dará el diccionario "mensaje": "Buscando {query}"
+    return respuesta.json() # Nos devolverá la respuesta del intermediario
